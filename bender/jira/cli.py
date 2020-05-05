c9e61777ab236dda8db83fd9e2e04bd7fbd54c7d
Charles Moreland <morelanc@ohsu.edu>
2020-05-05 09:31:46 -0700

    refactored

--- bender/jira/cli.py
+++ bender/jira/cli.py
@@ -1,5 +1,3 @@
-import json
-
 import click
 
 from bender.jira.cli_authconfig import jira_authconfig
@@ -8,6 +6,7 @@ from bender.jira.cli_index import jira_index
 from bender.jira.cli_property import jira_property
 from bender.jira.cli_session import jira_session
 from bender.jira.cli_webhook import jira_webhook
+from bender.jira.cli_user import jira_user
 from bender.utils import config, AppConnect, write_out, json_headers
 
 jira_config = config['jira']
@@ -24,7 +23,7 @@ jira_config = config['jira']
               help="output format")
 @click.pass_context
 def cli(ctx, server, username, password, output):
-    """Jira Server administration."""
+    """Server administration."""
     if not password:
         password = click.prompt(f'{server} password', hide_input=True, confirmation_prompt=True, show_default=False)
 
@@ -33,14 +32,6 @@ def cli(ctx, server, username, password, output):
                               cookie_store=jira_config.get('cookie_store')),
         'output': output
     }
-
-
-cli.add_command(jira_index)
-cli.add_command(jira_cluster)
-cli.add_command(jira_property)
-cli.add_command(jira_authconfig)
-cli.add_command(jira_webhook)
-cli.add_command(jira_session)
 
 
 @cli.command('status')
@@ -61,60 +52,33 @@ def jira_configuration(ctx):
     write_out(data=_res, output=ctx.obj['output'])
 
 
-@cli.command('user', no_args_is_help=True)
-@click.argument('name', default=None)
-@click.option('--password', default=None, type=str, help="Set Jira user password.")
+@cli.command('baseUrl')
+@click.option('--value', '-v', default=None, type=str, required=True, help="baseUrl of server.")
 @click.pass_context
-def jira_user(ctx, name, password):
-    """Jira user password.
-
-    name    Jira username.
-    \b
-    """
-    jira_user_path = '/rest/api/2/user'
-    if password:
-        params = {
-            'username': name
-        }
-        data = json.dumps({
-            'password': password
-        })
-        _res = ctx.obj['connect'].put(f'{jira_user_path}/password', headers=json_headers, params=params, data=data,
-                                      auth=True)
-        write_out(data=_res, output=ctx.obj['output'])
-    else:
-        params = {
-            'username': name
-        }
-        _res = ctx.obj['connect'].get(jira_user_path, headers=json_headers, params=params, auth=True)
-        write_out(data=_res, output=ctx.obj['output'])
+def jira_baseurl(ctx, value):
+    """Set Jira baseUrl."""
+    jira_settings_path = "rest/api/2/settings"
+    _res = ctx.obj['connect'].put(f'{jira_settings_path}/baseUrl', data=value, auth=True)
+    write_out(data=_res, output=ctx.obj['output'])
 
 
 @cli.command('serverinfo', no_args_is_help=True)
-@click.argument('action', type=click.Choice(['get', 'baseUrl']), default='get')
-@click.option('--value', default=None, type=str, help="value to set.")
-@click.option('--health-check', is_flag=True, default=False, help="run a health check")
+@click.option('--health-check', '-h', is_flag=True, default=False, help="run a health check")
 @click.pass_context
-def jira_serverinfo(ctx, action, value, health_check):
-    """Jira server information.
-
-    \b
-    get         view serverinfo.
-    baseUrl     set baseUrl to --value <url>.
-    """
+def jira_serverinfo(ctx, health_check):
+    """Jira server information and health-check."""
     jira_serverinfo_path = "rest/api/2/serverInfo"
-    jira_settings_path = "rest/api/2/settings"
-    if action is 'get':
-        params = {
-            'doHealthCheck': health_check
-        }
-        _res = ctx.obj['connect'].get(jira_serverinfo_path, params=params, headers=json_headers, auth=True)
-        write_out(data=_res, output=ctx.obj['output'])
+    params = {
+        'doHealthCheck': health_check
+    }
+    _res = ctx.obj['connect'].get(jira_serverinfo_path, params=params, headers=json_headers, auth=True)
+    write_out(data=_res, output=ctx.obj['output'])
 
-    if action is 'baseUrl':
-        if not value:
-            click.echo('--value required to set baseUrl')
-            exit(1)
 
-        _res = ctx.obj['connect'].put(f'{jira_settings_path}/baseUrl', data=value, auth=True)
-        write_out(data=_res, output=ctx.obj['output'])
+cli.add_command(jira_index)
+cli.add_command(jira_cluster)
+cli.add_command(jira_property)
+cli.add_command(jira_authconfig)
+cli.add_command(jira_webhook)
+cli.add_command(jira_session)
+cli.add_command(jira_user)
---
+++ bender/jira/cli_user.py
@@ -0,0 +1,46 @@
+import json
+
+import click
+
+from bender.utils import json_headers, write_out
+
+
+@click.group('user')
+@click.pass_context
+def jira_user(ctx):
+    """Jira user."""
+    pass
+
+
+@jira_user.command('get', no_args_is_help=True)
+@click.argument('name', default=None)
+@click.pass_context
+def jira_user_get(ctx, name):
+    """Get user."""
+    jira_user_path = '/rest/api/2/user'
+    params = {
+        'username': name
+    }
+    _res = ctx.obj['connect'].get(jira_user_path, headers=json_headers, params=params, auth=True)
+    write_out(data=_res, output=ctx.obj['output'])
+
+
+@jira_user.command('user', no_args_is_help=True)
+@click.argument('name', default=None)
+@click.option('--password', default=None, required=True, type=str, help="New user password.")
+@click.pass_context
+def jira_user_password(ctx, name, password):
+    """Set user password.
+
+    name    Jira username.
+    \b
+    """
+    jira_user_path = '/rest/api/2/user'
+    params = {
+        'username': name
+    }
+    data = json.dumps({
+        'password': password
+    })
+    _res = ctx.obj['connect'].put(f'{jira_user_path}/password', headers=json_headers, params=params, data=data, auth=True)
+    write_out(data=_res, output=ctx.obj['output'])
