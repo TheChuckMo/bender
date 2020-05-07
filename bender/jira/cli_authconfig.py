258617259e4d87ce01bf01434ccde9a3f9b3ab51
Charles Moreland <morelanc@ohsu.edu>
2020-05-07 11:14:16 -0700

    authconfig set and load

--- bender/jira/cli_authconfig.py
+++ bender/jira/cli_authconfig.py
@@ -1,6 +1,7 @@
 import click
 
 from bender.utils import json_headers
+import json
 
 
 @click.group('authconfig')
@@ -20,9 +21,26 @@ def jira_authconfig_get(ctx):
 
 
 @jira_authconfig.command('set')
-@click.option('--value', default=None, required=True, type=str, help="json value for authconfig")
+@click.option('--value', '-v', default=None, required=True, type=str, help="value to set")
+@click.option('--key', '-k', default=None, required=True, type=str, help="key to set")
 @click.pass_context
-def jira_authconfig_set(ctx, action, value):
+def jira_authconfig_set(ctx, key, value):
     """set authentication config."""
     jira_authconfig_path = "rest/authconfig/1.0/saml"
-    click.echo('not yet implemented.')
+    data = json.dumps({
+        key: value
+    })
+    _res = ctx.obj['connect'].put(jira_authconfig_path, headers=json_headers, data=data, auth=True)
+    ctx.obj['writer'].out(_res)
+
+
+@jira_authconfig.command('load')
+@click.argument('file', type=click.File('r'))
+@click.pass_context
+def jira_authconfig_load(ctx, file):
+    """set authentication config."""
+    jira_authconfig_path = "rest/authconfig/1.0/saml"
+    data = json.dumps(json.load(file))
+    #click.echo(data)
+    _res = ctx.obj['connect'].put(jira_authconfig_path, headers=json_headers, data=data, auth=True)
+    ctx.obj['writer'].out(_res)
